2db757ac3dea9c0c02a3b71f127da9d989cc0d3f
Asama Victor <hyxenaetechnology@gmail.com>
2024-11-15 16:07:02 +0100

    React template using nextjs framework

---
+++ src/components/index.js
@@ -0,0 +1,14 @@
+export function Header() {
+    return (
+        <button>This is a button</button>
+    )
+}
+
+export default function HeaderLink() {
+    return (
+        <div>
+            <Header />
+            <h1>This is an header tag</h1>
+        </div>
+    )
+}
